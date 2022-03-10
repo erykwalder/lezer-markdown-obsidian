85b4a34611b189ab947b8195e4c626af51a83cc0
Eric Rykwalder <e.rykwalder@gmail.com>
2022-03-09 22:03:01 -0500

    add rollup building

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "./src/index.ts",
+  output: [
+    {
+      format: "cjs",
+      file: "./dist/index.cjs",
+      externalLiveBindings: false,
+    },
+    {
+      format: "es",
+      file: "./dist/index.js",
+      externalLiveBindings: false,
+    },
+  ],
+  external: ["@lezer/common", "@lezer/markdown"],
+  plugins: [
+    typescript({
+      check: false,
+      tsconfigOverride: {
+        compilerOptions: {
+          lib: ["es5", "es6"],
+          sourceMap: true,
+          target: "es6",
+          strict: false,
+          declaration: true,
+        },
+        exclude: ["__tests__/*"],
+      },
+      include: ["src/*.ts"],
+    }),
+  ],
+};
